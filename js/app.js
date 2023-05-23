d433349cbe2472a40c461c6a6ea1e0d0d23a9253
avcristian <avilescristian97@gmail.com>
2023-05-22 22:40:47 -0300

    update

--- js/app.js
+++ js/app.js
@@ -1,20 +1,5 @@
-let productos = [];
-productos.push(new Burger('Simple Bacon', 1500));
-productos.push(new Burger('Doble Bacon', 2000));
-productos.push(new Burger('Simple Cheddar', 1300));
-productos.push(new Burger('Doble Cheddar', 1800));
-productos.push(new Burger('Simple Frany', 1500));
-productos.push(new Burger('Doble Frany', 2000));
+const carrito = document.getElementById('carrito');
+const burgers = document.getElementById('lista-burgers');
+const listaCarrito = document.getElementById('lista-carrito');
+const vaciarCarrito = document.getElementById('vacia-carrito');
 
-localStorage.setItem('listaBurger', JSON.stringify(productos));
-
-
-const listaProductos = document.getElementById('lista-productos');
-const listaCarrtio = document.getElementById('lista-carrito');
-const totalCarrito = document.getElementById('total-carrito');
-
-
-let carrito = [];
-
-listaProductos.addEventListener('click', agregarAlCarrito);
-cargarProductos();
--- js/funciones.js
+++ js/funciones.js
@@ -1,41 +1,13 @@
-function cargarProductos(){
-    const productos = JSON.parse(localStorage.getItem('listaBurger'));
-    if(productos){
-        carrito = productos;
-        mostrarCarrito();
+function eventLiteners(){
+    burgers.addEventListener('click', comprarBurger);
+}
+
+function comprarBurger(e){
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')){
+        const curso = e.target.parentElement.parentElement;
+        leerDatosBurger(curso);
     }
 }
 
-
-
-function agregarAlCarrito(event){
-    if(event.target.tagName === 'LI'){
-        const producto = {
-            nombre: event.target.getAttribute('date-nombre'),
-            precio: Number(event.target.getAttribute('data-precio'))
-        }
-        carrito.push(producto);
-        guardarPorductos();
-        mostrarCarrito();
-    }
-}
-
-function guardarPorductos(){
-    localStorage.setItem('carrito', JSON.stringify(carrito));
-}
-
-function mostrarCarrito(){
-    listaCarrtio.innerHTML = '';
-    carrito.forEach((producto) => {
-        const li = document.createElement('li');
-        li.textContent = `${producto.nombre} - $${producto.precio}`;
-        listaCarrtio.appendChild(li);
-    });
-    actualizarTotal();
-}
-
-function actualizarTotal() {
-    const total = carrito.reduce((acumular, producto) => acumular + producto.precio, 0);
-    totalCarrito.textContent = `$${total}`;
-}
-
+function leerDatosBurger()
